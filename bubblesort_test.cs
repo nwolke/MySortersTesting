cc552e06588af3d89f2551bc51d5fc18eba754a0
Nathan Wolke <nathanwolke@outlook.com>
2017-08-16 16:49:57 -0700

    formating

--- bubblesort_test.cs
+++ bubblesort_test.cs
@@ -29,15 +29,15 @@ public class BubbleSorter
 
     public static void FoundBubbleSortTest(ref int[] arr){
         int temp = 0;
-                for (int write = 0; write < arr.Length; write++) {
-                    for (int sort = 0; sort < arr.Length - 1; sort++) {
-                        if (arr[sort] > arr[sort + 1]) {
-                        temp = arr[sort + 1];
-                        arr[sort + 1] = arr[sort];
-                        arr[sort] = temp;
-                        }
-                    }
+        for (int write = 0; write < arr.Length; write++) {
+            for (int sort = 0; sort < arr.Length - 1; sort++) {
+                if (arr[sort] > arr[sort + 1]) {
+                    temp = arr[sort + 1];
+                    arr[sort + 1] = arr[sort];
+                    arr[sort] = temp;
                 }
+            }
+        }
     }
 
     public static string printArray(int[] arr){
